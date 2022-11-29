602a98fa44820976a91c5720548e9674681f2c04
nhatnm3399 <nhatnm3399@gmail.com>
2022-11-30 01:04:49 +0700

    upload

--- src/api/manage/update_hotel.js
+++ src/api/manage/update_hotel.js
@@ -8,7 +8,7 @@ const update_hotel= async (hotel_name, description, address, phone, image, image
         url: SERVER_URL+ "/api/hotel/update/"+ hotelId,
         method: "put",
         data: {
-            hotel_name, description, address, phone, image, image1, image2, image3, image4, city_id, latitude, longitude, check_in_time, check_out_time, is_payment_card, hotel_properties
+            hotel_name, description, address, phone, image, image1, image2, image3, image4, city_id, latitude, longitude, check_in_time, check_out_time, is_payment_card: is_payment_card=== true ? "1" : "0", hotel_properties
         },
         headers: {
             "authorization": "Bearer "+ Cookies.get("accessToken")
